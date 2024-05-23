a0fd5168cc361a78de4bc715932b208ec1c4d1cd
nhatduy2210 <tenduyhole@gmail.com>
2024-05-23 16:55:30 +0700

    update check register
    check email exist

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -12,6 +12,11 @@ async  register(payload : {email:string, password:string}){
     )
 return result
   }
+  async checkEmailExist(email:string){
+    const user = await databaseService.users.findOne({email})
+    console.log(user)
+    return Boolean(user)
+  }
 }
 
 const usersService = new UsersService()
--- src/📂middlewares/users.middlewares.ts
+++ src/📂middlewares/users.middlewares.ts
@@ -1,6 +1,8 @@
 import { error } from 'console'
 import { Request, Response, NextFunction } from 'express'
 import { checkSchema } from 'express-validator'
+import databaseService from '~/services/database.services'
+import usersService from '~/services/users.services'
 import { validate } from '~/utils/validation'
 export const loginValidator = (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body
@@ -12,78 +14,88 @@ export const loginValidator = (req: Request, res: Response, next: NextFunction) 
   next()
 }
 
-export const registerValidator = validate(checkSchema({
-  name: {
-    notEmpty: true,
-    isString: true,
-    isLength: {
-      options: {
-        min: 1,
-        max: 100
-      }
-    },
-    trim: true
-  },
-  email: {
-    notEmpty: true,
-    isEmail: true,
-    trim: true
-  },
-  password: {
-    notEmpty: true,
-    isString: true,
-    isLength: {
-      options: {
-        min: 6,
-        max: 50
-      }
-    },
-    isStrongPassword:{
-      options:{
-        minLength:6,
-        minLowercase:1,
-        minUppercase:1,
-        minSymbols:1
+export const registerValidator = validate(
+  checkSchema({
+    name: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: {
+          min: 1,
+          max: 100
+        }
       },
-      errorMessage:'Password cần ít nhất 6 kí tự và chứa ít nhất 1 chữ hoa thường 1 kí tự'
-    }
-  },
-  confirm_password:{
-    notEmpty: true,
-    isString: true,
-    isLength: {
-      options: {
-        min: 6,
-        max: 50
+      trim: true
+    },
+    email: {
+      notEmpty: true,
+      isEmail: true,
+      trim: true,
+      custom: {
+        options: async (value) => {
+          const isExxistEmail = await usersService.checkEmailExist(value)
+          if(isExxistEmail){
+            throw new Error('Email đã đăng ký')
+          }
+          return true
+        }
       }
     },
-    isStrongPassword:{
-      options:{
-        minLength:6,
-        minLowercase:1,
-        minUppercase:1,
-        minSymbols:1
+    password: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: {
+          min: 6,
+          max: 50
+        }
       },
-       
-      errorMessage:'Password cần ít nhất 6 kí tự và chứa ít nhất 1 chữ hoa thường 1 kí tự'
+      isStrongPassword: {
+        options: {
+          minLength: 6,
+          minLowercase: 1,
+          minUppercase: 1,
+          minSymbols: 1
+        },
+        errorMessage: 'Password cần ít nhất 6 kí tự và chứa ít nhất 1 chữ hoa thường 1 kí tự'
+      }
     },
-    custom:{
-      options:(value,{req})=>{
-if(value != req.body.password){
-  throw new Error('Không trùng khớp')
-}
-return true
+    confirm_password: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: {
+          min: 6,
+          max: 50
+        }
+      },
+      isStrongPassword: {
+        options: {
+          minLength: 6,
+          minLowercase: 1,
+          minUppercase: 1,
+          minSymbols: 1
+        },
+
+        errorMessage: 'Password cần ít nhất 6 kí tự và chứa ít nhất 1 chữ hoa thường 1 kí tự'
+      },
+      custom: {
+        options: (value, { req }) => {
+          if (value != req.body.password) {
+            throw new Error('Không trùng khớp')
+          }
+          return true
+        }
+      }
+    },
+    date_of_birth: {
+      //iso8601
+      isISO8601: {
+        options: {
+          strict: true,
+          strictSeparator: true
+        }
       }
     }
-  },
-  date_of_birth:{
-    //iso8601
-    isISO8601:{
-      options:{
-        strict:true,
-        strictSeparator:true
-      }
-    }
-  }
-   
-}))
+  })
+)
