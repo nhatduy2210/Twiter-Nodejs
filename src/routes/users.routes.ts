edb3efe57a1e7545fff8a2b8ef9d64326c227225
nhatduy2210 <tenduyhole@gmail.com>
2024-05-23 12:22:24 +0700

    update checkSchema Register
    fix validation

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
+import { validate } from "~/utils/validation";
 import { loginController,registerController } from "~/📂controllers/users.controllers";
 const usersRouter = Router()
 
-import { loginValidator } from "~/📂middlewares/users.middlewares";
+import { loginValidator, registerValidator } from "~/📂middlewares/users.middlewares";
 usersRouter.post('/login',loginValidator,loginController)
 //Description: Register a new users
 //path:/register
 //method:Post
 //Body:{name:string,email:string,password:string,confirm-passwordLstring, date_of_birth:ISO8601}
 
-usersRouter.post('/register',registerController)
+usersRouter.post('/register',validate(registerValidator),registerController)
 
 export default usersRouter
--- src/📂middlewares/users.middlewares.ts
+++ src/📂middlewares/users.middlewares.ts
@@ -1,16 +1,77 @@
-import { error } from "console";
-import { Request, Response,NextFunction } from "express";
-import { checkSchema } from "express-validator";
-export const loginValidator=(req:Request,res:Response,next:NextFunction) =>{
-  const{email,password}= req.body;
-  if(!email || !password) {
+import { error } from 'console'
+import { Request, Response, NextFunction } from 'express'
+import { checkSchema } from 'express-validator'
+export const loginValidator = (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body
+  if (!email || !password) {
     return res.status(400).json({
-      error:'missing email or password'
-    });
+      error: 'missing email or password'
+    })
   }
-  next();
+  next()
 }
 
 export const registerValidator = checkSchema({
-  
+  name: {
+    notEmpty: true,
+    isString: true,
+    isLength: {
+      options: {
+        min: 1,
+        max: 100
+      }
+    },
+    trim: true
+  },
+  email: {
+    notEmpty: true,
+    isEmail: true,
+    trim: true
+  },
+  password: {
+    notEmpty: true,
+    isString: true,
+    isLength: {
+      options: {
+        min: 6,
+        max: 50
+      }
+    },
+    isStrongPassword:{
+      options:{
+        minLength:6,
+        minLowercase:1,
+        minUppercase:1,
+        minSymbols:1
+      }
+    }
+  },
+  confirm_password:{
+    notEmpty: true,
+    isString: true,
+    isLength: {
+      options: {
+        min: 6,
+        max: 50
+      }
+    },
+    isStrongPassword:{
+      options:{
+        minLength:6,
+        minLowercase:1,
+        minUppercase:1,
+        minSymbols:1
+      }
+    }
+  },
+  date_of_birth:{
+    //iso8601
+    isISO8601:{
+      options:{
+        strict:true,
+        strictSeparator:true
+      }
+    }
+  }
+   
 })
