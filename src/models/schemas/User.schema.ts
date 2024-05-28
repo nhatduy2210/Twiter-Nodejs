3ac33efe434eae46a140536fc0551937e8b461ab
nhatduy2210 <tenduyhole@gmail.com>
2024-05-28 14:54:31 +0700

    update register
    apply accesstoken, refreshtoken, add info env, use jsonwebtoken

--- src/models/schemas/User.schema.ts
+++ src/models/schemas/User.schema.ts
@@ -1,16 +1,12 @@
 import { ObjectId } from "mongodb";
+import { UserVerifyStatus } from "~/constants/enums";
 
-enum UserVerifyStatus{
-  Unverified,
-  Verified,
-  Banned
-}
 
 interface UserType{
   _id?:ObjectId
-  name?:string
+  name:string
   email:string
-  date_of_birth?: Date
+  date_of_birth: Date
   password : string
   created_at? : Date
   updated_at?: Date
--- src/📂controllers/users.controllers.ts
+++ src/📂controllers/users.controllers.ts
@@ -1,8 +1,10 @@
 import { error } from 'console'
+import {ParamsDictionary} from 'express-serve-static-core'
 import { Request, Response, NextFunction } from 'express'
 import User from '~/models/schemas/User.schema'
 import databaseService from '~/services/database.services'
 import usersService from '~/services/users.services'
+import { RegisterReqBody } from '~/models/requests/User.requests'
 //xử lý logic
 export const loginController = (req: Request, res: Response) => {
   const { email, password } = req.body
@@ -17,10 +19,11 @@ export const loginController = (req: Request, res: Response) => {
   })
 }
 
-export const registerController = async (req: Request, res: Response) => {
+export const registerController = async (req: Request<ParamsDictionary,
+    any,RegisterReqBody>, res: Response) => {
   const { email, password } = req.body
   try {
-  const result = await usersService.register({email,password})
+    const result = await usersService.register(req.body)
     return res.json({
       message: 'regis sucess',
       result
